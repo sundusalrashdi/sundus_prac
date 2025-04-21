510a9ce22c58567450b88ff7ba5a9748fc4ee819
sundusalrashdi <sundusah3535@gmail.com>
2025-04-21 10:10:38 +0400

    Hello World

---
+++ main_app.py
@@ -0,0 +1,15 @@
+num_1= input("Enter 1st Number: ")
+num_2= input("Enter 2nd Number: ")
+opreation = input("Select + or - :")
+result =0 
+
+
+if opreation == "+":
+    result = num_1 + num_2
+
+elif opreation == "-":
+    result = num_1 - num_2
+
+
+
+print ("Result is:", result) 
---
+++ variables_practice.py
@@ -0,0 +1,46 @@
+'''
+number= 2 
+
+for c in range(5):
+    m= number * (c+1)
+    
+    print(str(number)+ "x" + str(c+1) +"="+str(m) )
+
+
+print("End--------End") 
+'''
+
+""""
+num_of_rows = 5 
+
+for c in range(num_of_rows):
+    print('*****')
+    
+
+
+print("End--------End") 
+"""
+
+'''
+num_of_rows = 5
+
+for i in range(num_of_rows):
+    for j in range(2 * i + 1):
+        print('*',end='')
+    print()
+'''
+'''
+print('\"Hello Team Code Orbit"') 
+'''
+
+
+No_of_rows = 3
+No_of_columns = 3
+counter = 0
+for row in range(No_of_rows):
+  for col in range(No_of_columns):
+    counter = counter + 1
+    print(counter, end="")
+  print("") 
+
+
