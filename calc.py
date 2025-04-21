99bea72289d45f137db73782c846cf0c69cc540e
sundusalrashdi <sundusah3535@gmail.com>
2025-04-21 10:29:06 +0400

    power

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 num_1= int(input("Enter 1st Number: "))
 num_2= int(input("Enter 2nd Number: "))
-opreation = input("Select + or - or / or * :")
+opreation = input("Select + or - or / or * or ** :")
 result =0 
 
 
@@ -15,6 +15,9 @@ elif opreation == "/":
 
 elif opreation == "*":
     result = num_1 * num_2
+
+elif opreation == "**":
+    result = num_1 ** num_2
 
 
 
