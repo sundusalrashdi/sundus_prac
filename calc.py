c22ad05fa3667cce0610c5cfb07c4e0267d7d6a3
sundusalrashdi <sundusah3535@gmail.com>
2025-04-21 10:22:25 +0400

    multi and div

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 num_1= int(input("Enter 1st Number: "))
 num_2= int(input("Enter 2nd Number: "))
-opreation = input("Select + or - :")
+opreation = input("Select + or - or / or * :")
 result =0 
 
 
@@ -9,6 +9,12 @@ if opreation == "+":
 
 elif opreation == "-":
     result = num_1 - num_2
+
+elif opreation == "/":
+    result = num_1 / num_2 
+
+elif opreation == "*":
+    result = num_1 * num_2
 
 
 
