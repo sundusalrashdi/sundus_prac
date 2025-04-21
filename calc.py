ac9bcb6333445a0713f1a97a666bfea35ded2707
sundusalrashdi <sundusah3535@gmail.com>
2025-04-21 15:11:01 +0400

    AND

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 num_1= int(input("Enter 1st Number: "))
 num_2= int(input("Enter 2nd Number: "))
-opreation = input("Select + or - or / or * or ** or ! or absolute or (OR) | :")
+opreation = input("Select + or - or / or * or ** or ! or absolute or (OR) | or (AND) & :") 
 result =0 
 
 
@@ -31,8 +31,7 @@ elif opreation == "absolute":
 elif opreation == "|":
      result = num_1 | num_2 
 
-     
- 
-
+elif opreation == "&":
+     result = num_1 & num_2 
 
 print ("Result is:", result) 
