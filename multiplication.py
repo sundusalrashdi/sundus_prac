ab2c02fa02359d749b227658bfeaf80c3bd34684
Tufool98 <tufool.hn@hotmail.com>
2025-04-22 15:39:03 +0400

    add multi results

--- multiplication.py
+++ multiplication.py
@@ -1,6 +1,7 @@
 num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: ")) 
 num_3 = int(input("Enter third number: ")) 
-result = num_1 * num_2 * num_3 
-
-print("result: ",result) 
+result1 = num_1 * num_2 * num_3 
+result2 = num_1 * num_2 + num_3 
+print("result1: ",result1) 
+print("result2: ",result2) 
