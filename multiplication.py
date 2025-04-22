214978ea3f8f74fc28b70cde92da9d93d1a32130
sundusalrashdi <sundusah3535@gmail.com>
2025-04-22 15:05:55 +0400

    multi num3

--- multiplication.py
+++ multiplication.py
@@ -1,6 +1,6 @@
 num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: ")) 
 num_3 = int(input("Enter third number: ")) 
-result = num_1 * num_2 
+result = num_1 * num_2 * num_3 
 
 print("result: ",result) 
