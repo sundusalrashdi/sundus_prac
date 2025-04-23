31e20e1695dba8735272202e90f349127e661787
sundusalrashdi <sundusah3535@gmail.com>
2025-04-23 11:53:18 +0400

    result 3 abs

--- multiplication.py
+++ multiplication.py
@@ -3,9 +3,9 @@ num_2 = int(input("Enter second number: "))
 num_3 = int(input("Enter third number: ")) 
 result1 = num_1 * num_2 * num_3 
 result2 = num_1 * num_2 + num_3 
-
+result3 = num_1 % num_2 
 result4 = num_1 / num_3
 print("result1: ",result1) 
 print("result2: ",result2) 
-
-print("result4:",result4)
+print("result3: ",result3) 
+print("result4:",result4) 
