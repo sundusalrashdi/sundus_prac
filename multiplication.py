7076be0f5bac264f6f4d8fdf30bad9399ae75d4d
Hanan170 <han.aljabri@gmail.com>
2025-04-23 11:44:32 +0400

    division

--- multiplication.py
+++ multiplication.py
@@ -3,5 +3,9 @@ num_2 = int(input("Enter second number: "))
 num_3 = int(input("Enter third number: ")) 
 result1 = num_1 * num_2 * num_3 
 result2 = num_1 * num_2 + num_3 
+
+result4 = num_1 / num_3
 print("result1: ",result1) 
 print("result2: ",result2) 
+
+print("result4:",result4)
